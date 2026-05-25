3bcbe9dba747822cbc5957e4527dec5ccb06f7a8
aguiro16 <aguiro16@gmail.com>
2026-05-25 05:41:29 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BINANCE_BASE_URL         = "https://api.binance.com"
-BINANCE_FUTURES_BASE_URL = "https://fapi.binance.com"
-
-TELEGRAM_TOKEN    = os.environ.get("TELEGRAM_TOKEN", "")
-TELEGRAM_CHAT_ID  = os.environ.get("TELEGRAM_CHAT_ID", "")
-ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
